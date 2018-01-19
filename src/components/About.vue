744a76501f6bca7e4e7d214865e51a13e1a1dfd6
ParkYoungWoong <thesecon@gmail.com>
2018-01-19 11:05:07 +0900

    Testing router with '/about'.

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Flex Help me!</h1>
+    <p>This web page was created to help you understand CSS Flexbox tech.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'About'
+  }
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import FlexMain from '@/components/FlexMain'
+import About from '@/components/About'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'FlexMain',
       component: FlexMain
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
     }
   ]
 })
